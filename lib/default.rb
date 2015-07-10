89e5d869e996961cc6dca026fa6f9306483996ab
Zach Aysan <zachaysan@gmail.com>
2015-07-10 11:41:49 -0400

    Include CacheBusting

--- lib/default.rb
+++ lib/default.rb
@@ -1,3 +1,6 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+require 'nanoc/cachebuster'
+
 include Nanoc::Helpers::LinkTo
+include Nanoc3::Helpers::CacheBusting
